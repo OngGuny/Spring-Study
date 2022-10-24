38a45a7e9fb877412336369d9eb2a2d59e28309d
OngGuny <start1step@naver.com>
2022-10-24 19:39:32 +0900

    1024 class Stuffs

---
+++ SpringTest2/src/test/java/kr/kwangan2/test/TimeMapperTests.java
@@ -0,0 +1,31 @@
+package kr.kwangan2.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.kwangan2.mapper.TimeMapper;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class TimeMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}// test1
+	@Test
+	public void testGetTime2() {
+		log.info("getter2");
+		log.info(timeMapper.getTime2());
+	}
+	
+}//class
